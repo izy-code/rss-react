7e6246a464848d6ae660a7a6d16becc52b2db0b6
Izy <126877709+izy-code@users.noreply.github.com>
2024-07-07 21:42:44 +0300

    feat: add SearchForm

---
+++ src/components/search-form/SearchForm.tsx
@@ -0,0 +1,49 @@
+import type { FormEvent, ReactNode } from 'react';
+import { Component } from 'react';
+
+import { CustomButton } from '../custom-button/CustomButton';
+import styles from './styles.module.scss';
+
+type Props = {
+  onSearch: (term: string) => void;
+  initialTerm: string;
+  isLoading: boolean;
+};
+
+type State = {
+  searchTerm: string;
+};
+
+export class SearchForm extends Component<Props, State> {
+  private handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+
+    if (event.target instanceof HTMLFormElement) {
+      const { onSearch } = this.props;
+      const formData = new FormData(event.target);
+
+      onSearch(formData.get('search')?.toString().trim() || '');
+    }
+  };
+
+  public render(): ReactNode {
+    const { isLoading, initialTerm } = this.props;
+
+    return (
+      <form className={styles.form} onSubmit={this.handleSubmit}>
+        <input
+          className={styles.input}
+          type="search"
+          placeholder="Enter character name…"
+          defaultValue={initialTerm}
+          name="search"
+          disabled={isLoading}
+          autoComplete="off"
+        />
+        <CustomButton type="submit" variant="secondary" isDisabled={isLoading}>
+          Search
+        </CustomButton>
+      </form>
+    );
+  }
+}
