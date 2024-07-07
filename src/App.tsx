3564173a87f730497a61e8fb5e841e234b5023da
Izy <126877709+izy-code@users.noreply.github.com>
2024-07-07 21:35:43 +0300

    fix: add MainPage to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,10 @@
-import './App.scss';
+import type { ReactNode } from 'react';
+import { Component } from 'react';
 
-import { useState } from 'react';
+import { MainPage } from '@/pages/main-page/MainPage';
 
-import reactLogo from '@/assets/react.svg';
-import viteLogo from '@/assets/vite.svg';
-
-function App(): JSX.Element {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((prevCount) => prevCount + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  );
+export class App extends Component {
+  public render(): ReactNode {
+    return <MainPage />;
+  }
 }
-
-export default App;
