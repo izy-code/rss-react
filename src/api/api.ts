a4d9f86e232e6177510d00e480372400f34f826f
Izy <126877709+izy-code@users.noreply.github.com>
2024-07-07 21:44:13 +0300

    feat: add api methods

---
+++ src/api/api.ts
@@ -0,0 +1,27 @@
+import type { ApiResponse } from './types';
+
+const BASE_URL = 'https://rickandmortyapi.com/api/character';
+const DEFAULT_PAGE = 1;
+
+export const fetchCharacters = async (searchTerm: string, page = DEFAULT_PAGE): Promise<ApiResponse> => {
+  const params = new URLSearchParams({
+    page: page.toString(),
+  });
+
+  if (searchTerm) {
+    params.append('name', searchTerm);
+  }
+
+  const url = `${BASE_URL}/?${params.toString()}`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    if (response.status === 404) {
+      return null;
+    }
+
+    throw new Error('Network response was not ok');
+  }
+
+  return response.json() as Promise<ApiResponse>;
+};
