5d63295d78cfb61f9464d63cf3dd832d8c9e5c5a
Izy <126877709+izy-code@users.noreply.github.com>
2024-07-13 23:02:48 +0300

    fix: del alias config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-import path from 'node:path';
-
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
@@ -7,9 +5,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
-  },
   plugins: [
     react(),
     tsconfigPaths(),
