94be40947fae701b20f5b46b9d1ac07cbfd68104
Izy <126877709+izy-code@users.noreply.github.com>
2024-07-25 22:17:16 +0300

    fix: change theme styles using :has selector

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,8 +1,11 @@
+import clsx from 'clsx';
 import type { ReactNode } from 'react';
-import { createContext, useCallback, useEffect, useLayoutEffect, useMemo, useState } from 'react';
+import { createContext, useCallback, useEffect, useMemo, useState } from 'react';
 
 import { LocalStorageKeys } from '@/common/enums';
 import { useLocalStorage } from '@/hooks/useLocalStorage';
+
+import styles from './ThemeContext.module.scss';
 
 interface ThemeContextType {
   isDarkTheme: boolean;
@@ -20,14 +23,6 @@ export function ThemeProvider({ children }: { children: ReactNode }): ReactNode 
   const { getStoredValue, setStoredValue } = useLocalStorage<boolean>();
   const [isDarkTheme, setIsDarkTheme] = useState(getStoredValue(LocalStorageKeys.THEME) || false);
 
-  useLayoutEffect(() => {
-    if (isDarkTheme) {
-      document.body.classList.add(DARK_THEME_CLASS);
-    } else {
-      document.body.classList.remove(DARK_THEME_CLASS);
-    }
-  }, [isDarkTheme]);
-
   useEffect(() => {
     setStoredValue(LocalStorageKeys.THEME, isDarkTheme);
   }, [isDarkTheme, setStoredValue]);
@@ -36,5 +31,9 @@ export function ThemeProvider({ children }: { children: ReactNode }): ReactNode 
 
   const contextValue = useMemo(() => ({ isDarkTheme, toggleTheme }), [isDarkTheme, toggleTheme]);
 
-  return <ThemeContext.Provider value={contextValue}>{children}</ThemeContext.Provider>;
+  return (
+    <ThemeContext.Provider value={contextValue}>
+      <div className={clsx(styles.container, isDarkTheme ? DARK_THEME_CLASS : '')}>{children}</div>
+    </ThemeContext.Provider>
+  );
 }
