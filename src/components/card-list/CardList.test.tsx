518ba35bed9e01c3dc1b6b49c4fa0a2f0fdddabc
Izy <126877709+izy-code@users.noreply.github.com>
2024-07-27 22:08:14 +0300

    fix: del redundant fetch asserts

--- src/components/card-list/CardList.test.tsx
+++ src/components/card-list/CardList.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
 import { charactersDataMock } from '@/test/mocks/mocks';
@@ -15,32 +15,22 @@ describe('CardList Component', () => {
   });
 
   it('renders the specified number of cards', async (): Promise<void> => {
-    const fetchSpy = vi.spyOn(globalThis, 'fetch');
-
     renderWithProvidersAndUser(
       <MemoryRouter initialEntries={[`/?page=${MOCK_PAGE_NUMBER}&name=${MOCK_SEARCH_NAME}`]}>
         <CardList />
       </MemoryRouter>,
     );
-    await waitFor(() => {
-      expect(fetchSpy).toHaveBeenCalled();
-    });
+
     const cards = await screen.findAllByRole('listitem');
     expect(cards).toHaveLength(charactersDataMock.info.count);
   });
 
   it('displays an appropriate message if no cards are present', async (): Promise<void> => {
-    const fetchSpy = vi.spyOn(globalThis, 'fetch');
-
     renderWithProvidersAndUser(
       <MemoryRouter initialEntries={[`/?name=wrong`]}>
         <CardList />
       </MemoryRouter>,
     );
-
-    await waitFor(() => {
-      expect(fetchSpy).toHaveBeenCalled();
-    });
 
     const message = await screen.findByText('No characters found');
     expect(message).toBeInTheDocument();
--- src/components/details/Details.test.tsx
+++ src/components/details/Details.test.tsx
@@ -1,10 +1,9 @@
 import '@testing-library/jest-dom';
 
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 
 import { MainPage } from '@/pages/main-page/MainPage';
-import { BASE_URL } from '@/store/api/api-slice';
 import { characterMock } from '@/test/mocks/mocks';
 import { renderWithProvidersAndUser } from '@/utils/test-utils';
 
@@ -16,8 +15,6 @@ describe('Details Component', () => {
   });
 
   it('displays a loading indicator while fetching data', async () => {
-    const fetchSpy = vi.spyOn(globalThis, 'fetch');
-
     renderWithProvidersAndUser(
       <MemoryRouter initialEntries={[`/?details=${characterMock.id}`]}>
         <Details />
@@ -25,37 +22,22 @@ describe('Details Component', () => {
     );
 
     expect(await screen.findByRole('heading', { name: /loading.../i })).toBeInTheDocument();
-
-    await waitFor(() =>
-      expect(fetchSpy).toHaveBeenCalledWith(
-        expect.objectContaining({ url: `${BASE_URL}character/${characterMock.id}` }),
-      ),
-    );
   });
 
   it('displays the detailed card data correctly', async () => {
-    const fetchSpy = vi.spyOn(globalThis, 'fetch');
-
     renderWithProvidersAndUser(
       <MemoryRouter initialEntries={[`/?details=${characterMock.id}`]}>
         <Details />
       </MemoryRouter>,
     );
 
-    await waitFor(() =>
-      expect(fetchSpy).toHaveBeenCalledWith(
-        expect.objectContaining({ url: `${BASE_URL}character/${characterMock.id}` }),
-      ),
-    );
-
-    expect(screen.getByText(characterMock.name)).toBeInTheDocument();
+    expect(await screen.findByText(characterMock.name)).toBeInTheDocument();
     expect(screen.getByText(characterMock.species)).toBeInTheDocument();
     expect(screen.getByText(characterMock.status)).toBeInTheDocument();
     expect(screen.getByText(characterMock.gender)).toBeInTheDocument();
   });
 
   it('hides the details section when the close button is clicked', async () => {
-    const fetchSpy = vi.spyOn(globalThis, 'fetch');
     const { user } = renderWithProvidersAndUser(
       <MemoryRouter initialEntries={[`/?details=${characterMock.id}`]}>
         <Routes>
@@ -64,12 +46,6 @@ describe('Details Component', () => {
           </Route>
         </Routes>
       </MemoryRouter>,
-    );
-
-    await waitFor(() =>
-      expect(fetchSpy).toHaveBeenCalledWith(
-        expect.objectContaining({ url: `${BASE_URL}character/${characterMock.id}` }),
-      ),
     );
 
     const closeButton = await screen.findByRole('button', { name: /close details/i });
