2ba0353d3a36e486a354982055d55ac7cd4e72c5
Izy <126877709+izy-code@users.noreply.github.com>
2024-07-28 00:34:47 +0300

    feat: add flyout integration test to card list

--- src/components/card-list/CardList.test.tsx
+++ src/components/card-list/CardList.test.tsx
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom';
 import { screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
+import { SearchParams } from '@/common/enums';
 import { charactersDataMock } from '@/test/mocks/mocks';
 import { MOCK_PAGE_NUMBER, MOCK_SEARCH_NAME } from '@/test/msw/handlers';
 import { renderWithProvidersAndUser } from '@/utils/test-utils';
@@ -34,5 +35,35 @@ describe('CardList Component', () => {
 
     const message = await screen.findByText('No characters found');
     expect(message).toBeInTheDocument();
+  });
+
+  it('should select items and update flyout accordingly', async () => {
+    vi.mock('@/utils/utils', async () => {
+      const actual = await vi.importActual('@/utils/utils');
+      return {
+        ...actual,
+        getCsvObjectUrl: vi.fn(() => 'blob:fake-url'),
+      };
+    });
+
+    const { user } = renderWithProvidersAndUser(
+      <MemoryRouter initialEntries={[`/?${SearchParams.NAME}=${MOCK_SEARCH_NAME}`]}>
+        <CardList />
+      </MemoryRouter>,
+    );
+
+    const checkboxes = await screen.findAllByRole('checkbox');
+    expect(checkboxes.length).toBe(3);
+
+    checkboxes.forEach(async (checkbox) => {
+      await user.click(checkbox);
+    });
+
+    expect(await screen.findByText(/items selected: 3/i)).toBeInTheDocument();
+
+    const unselectAllButton = screen.getByRole('button', { name: /unselect all/i });
+    await user.click(unselectAllButton);
+
+    expect(screen.getByText(/items selected: 0/i)).toBeInTheDocument();
   });
 });
