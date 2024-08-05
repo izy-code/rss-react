add19328f76d900b94cdbb8aa638bd1f20ddf1c4
Izy <126877709+izy-code@users.noreply.github.com>
2024-08-05 18:53:45 +0300

    fix: update ThemeContext test

--- src/contexts/ThemeContext.test.tsx
+++ src/contexts/ThemeContext.test.tsx
@@ -1,5 +1,3 @@
-import '@testing-library/jest-dom';
-
 import { render, screen, waitFor } from '@testing-library/react';
 import type { ReactNode } from 'react';
 import { useContext } from 'react';
