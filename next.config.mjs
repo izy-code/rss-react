adddbfa0e7f22344f489b8f46921425484381abc
Izy <126877709+izy-code@users.noreply.github.com>
2024-08-05 11:31:38 +0300

    fix: move image assets to public folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
-
 const nextConfig = {
   reactStrictMode: true,
   distDir: './dist',
@@ -16,9 +10,6 @@ const nextConfig = {
         protocol: 'https',
       },
     ],
-  },
-  sassOptions: {
-    includePaths: [path.join(dirname, 'styles')],
   },
 };
 
