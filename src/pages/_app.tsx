69c700c0a74e72b0bc41dca211260b64f4253bac
Izy <126877709+izy-code@users.noreply.github.com>
2024-08-05 11:44:47 +0300

    feat: add Custom App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
+import '@/styles/global.scss';
+
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import type { ReactNode } from 'react';
+import { Provider } from 'react-redux';
+
+import { ErrorBoundary } from '@/components/error-boundary/ErrorBoundary';
+import { ThemeProvider } from '@/contexts/ThemeContext';
+import { wrapper } from '@/store/store';
 
 export default function App({ Component, pageProps }: AppProps): ReactNode {
+  const { store } = wrapper.useWrappedStore(pageProps);
+
   return (
     <>
       <Head>
@@ -11,13 +20,13 @@ export default function App({ Component, pageProps }: AppProps): ReactNode {
         <meta name="description" content="Next.JS Pages Router educational project at the RS School" />
         <link rel="icon" type="image/svg+xml" href="/rick-favicon.svg" />
       </Head>
-
-      {/* <ErrorBoundary>
-        {isLoading && <Loading />}
-        <Provider store={store}> */}
-      <Component {...pageProps} />
-      {/* </Provider>
-      </ErrorBoundary> */}
+      <ErrorBoundary>
+        <Provider store={store}>
+          <ThemeProvider>
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </Provider>
+      </ErrorBoundary>
     </>
   );
 }
