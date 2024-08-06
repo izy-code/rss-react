0989da55f37592d9852659fc5bf7306c69a9bbfe
Izy <126877709+izy-code@users.noreply.github.com>
2024-08-06 13:08:06 +0300

    fix: change page 500 component name

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -2,6 +2,6 @@ import type { ReactNode } from 'react';
 
 import ErrorStatusPage from '@/components/error-status-page/ErrorStatusPage';
 
-export default function Page404(): ReactNode {
+export default function Page500(): ReactNode {
   return <ErrorStatusPage status={500} message="Server-side error occurred." />;
 }
