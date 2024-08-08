43da066e06ccdf23a34fb4a4fdf4f61b8e366d52
Izy <126877709+izy-code@users.noreply.github.com>
2024-08-08 21:22:51 +0300

    fix: remove RTK query API slice

--- src/test/mocks/mocks.ts
+++ src/test/mocks/mocks.ts
@@ -1,4 +1,4 @@
-import type { CharacterData, CharacterListData } from '@/store/api/types';
+import type { CharacterData, CharacterListData } from '@/api/types';
 
 export const characterMock: CharacterData = {
   id: 1,
