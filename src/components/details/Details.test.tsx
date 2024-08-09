1d4140e79392e9039c808b70ef65af2ad448f866
Izy <126877709+izy-code@users.noreply.github.com>
2024-08-09 15:15:35 +0300

    fix: update details tests

--- src/components/details/Details.test.tsx
+++ src/components/details/Details.test.tsx
@@ -1,37 +1,40 @@
 import { screen } from '@testing-library/react';
 
 import { SearchParams } from '@/common/enums';
-import Home from '@/pages';
 import { characterMock } from '@/test/mocks/mocks';
-import { getMockedNextRouter, renderWithProvidersAndUser } from '@/utils/test-utils';
+import { renderWithProvidersAndUser } from '@/utils/test-utils';
 
 import { Details } from './Details';
+
+const mockedRouterPush = vi.fn();
+
+vi.mock('next/navigation', async () => {
+  const actual = await vi.importActual('next/navigation');
+  return {
+    ...actual,
+    useRouter: (): {
+      push: () => void;
+    } => ({ push: mockedRouterPush }),
+    useSearchParams: vi.fn(() => {
+      const searchParams = new URLSearchParams({ [SearchParams.DETAILS]: characterMock.id.toString() });
+      return searchParams;
+    }),
+  };
+});
 
 describe('Details Component', () => {
   afterEach(() => {
     vi.clearAllMocks();
   });
 
-  it('displays a loading indicator while fetching data', async () => {
-    const { NextProvider } = getMockedNextRouter({ query: { [SearchParams.DETAILS]: characterMock.id.toString() } });
-
-    renderWithProvidersAndUser(
-      <NextProvider>
-        <Details />
-      </NextProvider>,
-    );
+  it('displays a loading indicator while fetching image', async () => {
+    renderWithProvidersAndUser(<Details character={{ status: 'success', data: characterMock }} />);
 
     expect(await screen.findByRole('heading', { name: /loading.../i })).toBeInTheDocument();
   });
 
   it('displays the detailed card data correctly', async () => {
-    const { NextProvider } = getMockedNextRouter({ query: { [SearchParams.DETAILS]: characterMock.id.toString() } });
-
-    renderWithProvidersAndUser(
-      <NextProvider>
-        <Details />
-      </NextProvider>,
-    );
+    renderWithProvidersAndUser(<Details character={{ status: 'success', data: characterMock }} />);
 
     expect(await screen.findByText(characterMock.name)).toBeInTheDocument();
     expect(screen.getByText(characterMock.species)).toBeInTheDocument();
@@ -40,15 +43,7 @@ describe('Details Component', () => {
   });
 
   it('hides the details section when the close button is clicked by deleting the query param', async () => {
-    const { NextProvider, nextRouter } = getMockedNextRouter({
-      query: { [SearchParams.DETAILS]: characterMock.id.toString() },
-    });
-
-    const { user } = renderWithProvidersAndUser(
-      <NextProvider>
-        <Home />
-      </NextProvider>,
-    );
+    const { user } = renderWithProvidersAndUser(<Details character={{ status: 'success', data: characterMock }} />);
 
     const closeButton = await screen.findByRole('button', { name: /close details/i });
 
@@ -56,6 +51,6 @@ describe('Details Component', () => {
 
     await user.click(closeButton);
 
-    expect(nextRouter.push).toHaveBeenCalledWith({ query: {} });
+    expect(mockedRouterPush).toHaveBeenCalledWith('?');
   });
 });
