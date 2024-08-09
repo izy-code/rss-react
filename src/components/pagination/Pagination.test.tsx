241a1b320c49438363c6a355fe07136c703051fd
Izy <126877709+izy-code@users.noreply.github.com>
2024-08-09 15:16:01 +0300

    fix: update pagination tests

--- src/components/pagination/Pagination.test.tsx
+++ src/components/pagination/Pagination.test.tsx
@@ -1,11 +1,30 @@
 import { screen } from '@testing-library/react';
 import { vi } from 'vitest';
 
+import { AsyncWrapper } from '@/app/page';
 import { SearchParams } from '@/common/enums';
 import { charactersDataMock } from '@/test/mocks/mocks';
-import { getMockedNextRouter, renderWithProvidersAndUser } from '@/utils/test-utils';
+import { MOCK_PAGE_NUMBER, MOCK_SEARCH_NAME } from '@/test/msw/handlers';
+import { renderWithProvidersAndUser } from '@/utils/test-utils';
 
 import { Pagination } from './Pagination';
+
+const mockedRouterPush = vi.fn();
+let mockedPageNumber = +MOCK_PAGE_NUMBER;
+
+vi.mock('next/navigation', async () => {
+  const actual = await vi.importActual('next/navigation');
+  return {
+    ...actual,
+    useRouter: (): {
+      push: () => void;
+    } => ({ push: mockedRouterPush }),
+    useSearchParams: vi.fn(() => {
+      const searchParams = new URLSearchParams({ [SearchParams.PAGE]: mockedPageNumber.toString() });
+      return searchParams;
+    }),
+  };
+});
 
 describe('Pagination Component', () => {
   const pageInfoMock = charactersDataMock.info;
@@ -14,79 +33,44 @@ describe('Pagination Component', () => {
     vi.clearAllMocks();
   });
 
-  it('disables the "Prev" button on the first page', () => {
-    const initialPage = 1;
+  it('disables the "Prev" and "Next" button on the single page', async () => {
+    const Awaited = (await AsyncWrapper({
+      searchParams: {
+        [SearchParams.NAME]: MOCK_SEARCH_NAME,
+        [SearchParams.PAGE]: MOCK_PAGE_NUMBER,
+      },
+    })) as JSX.Element;
 
-    const { NextProvider } = getMockedNextRouter({ query: { [SearchParams.PAGE]: initialPage.toString() } });
+    renderWithProvidersAndUser(Awaited);
 
-    renderWithProvidersAndUser(
-      <NextProvider>
-        <Pagination pageInfo={pageInfoMock} />
-      </NextProvider>,
-    );
-
+    const nextButton = screen.getByRole('button', { name: /next/i });
     const prevButton = screen.getByRole('button', { name: /prev/i });
 
     expect(prevButton).toBeDisabled();
-  });
-
-  it('disables the "Next" button on the last page', () => {
-    const { NextProvider } = getMockedNextRouter({ query: { [SearchParams.PAGE]: pageInfoMock.pages.toString() } });
-
-    renderWithProvidersAndUser(
-      <NextProvider>
-        <Pagination pageInfo={pageInfoMock} />
-      </NextProvider>,
-    );
-
-    const nextButton = screen.getByRole('button', { name: /next/i });
-
     expect(nextButton).toBeDisabled();
   });
 
   it('updates URL query parameter when "Next" button is clicked', async () => {
-    const initialPage = 2;
+    mockedPageNumber = 2;
 
-    const { NextProvider, nextRouter } = getMockedNextRouter({
-      query: { [SearchParams.PAGE]: initialPage.toString() },
-    });
-
-    const { user } = renderWithProvidersAndUser(
-      <NextProvider>
-        <Pagination pageInfo={pageInfoMock} />
-      </NextProvider>,
-    );
+    const { user } = renderWithProvidersAndUser(<Pagination pageInfo={pageInfoMock} />);
 
     const nextButton = screen.getByRole('button', { name: /next/i });
 
     await user.click(nextButton);
 
-    expect(nextRouter.push).toHaveBeenCalledWith({
-      query: { [SearchParams.PAGE]: (initialPage + 1).toString() },
-    });
+    expect(mockedRouterPush).toHaveBeenCalledWith(`?page=${mockedPageNumber + 1}`);
   });
 
   it('updates URL query parameter when "Prev" button is clicked', async () => {
-    const initialPage = 2;
+    mockedPageNumber = 2;
 
-    const { NextProvider, nextRouter } = getMockedNextRouter({
-      query: { [SearchParams.PAGE]: initialPage.toString() },
-    });
-
-    const { user } = renderWithProvidersAndUser(
-      <NextProvider>
-        <Pagination pageInfo={pageInfoMock} />
-      </NextProvider>,
-    );
+    const { user } = renderWithProvidersAndUser(<Pagination pageInfo={pageInfoMock} />);
 
     const prevButton = screen.getByRole('button', { name: /prev/i });
 
     await user.click(prevButton);
 
-    expect(nextRouter.push).toHaveBeenCalledWith(
-      expect.objectContaining({
-        query: { [SearchParams.PAGE]: (initialPage - 1).toString() },
-      }),
-    );
+    expect(mockedRouterPush).toHaveBeenCalledWith(`?page=${mockedPageNumber - 1}`);
   });
 });
