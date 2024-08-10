e0ba1224a4a1f300ae3250f8d2f1c3e6237e9e4c
Izy <126877709+izy-code@users.noreply.github.com>
2024-08-10 16:46:12 +0300

    feat: add local storage handling on server side

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -4,7 +4,11 @@ import type { LocalStorageKeys } from '@/common/enums';
 
 export const LOCAL_STORAGE_KEY = 'izy-react-task-3';
 
-export function getLocalStorage<T>(): Record<string, T> {
+export function getLocalStorage<T>(): Record<string, T> | null {
+  if (typeof window === 'undefined') {
+    return null;
+  }
+
   const item = localStorage.getItem(LOCAL_STORAGE_KEY);
   return item ? (JSON.parse(item) as Record<string, T>) : {};
 }
@@ -13,9 +17,17 @@ export function useLocalStorage<T>(): {
   getStoredValue: (key: LocalStorageKeys) => T | null;
   setStoredValue: (key: LocalStorageKeys, value: T) => void;
 } {
-  const getStoredValue = useCallback((key: LocalStorageKeys): T | null => getLocalStorage<T>()[key] ?? null, []);
+  const getStoredValue = useCallback((key: LocalStorageKeys): T | null => {
+    const localStorageMap = getLocalStorage<T>();
+
+    return localStorageMap?.[key] || null;
+  }, []);
 
   const setStoredValue = useCallback((key: LocalStorageKeys, value: T) => {
+    if (typeof window === 'undefined') {
+      return;
+    }
+
     let localStorageMap = getLocalStorage<T>();
 
     localStorageMap = { ...localStorageMap, [key]: value };
