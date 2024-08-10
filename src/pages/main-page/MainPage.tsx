3fd116dca68c8133f7c3bf16244e5cca1e983a72
Izy <126877709+izy-code@users.noreply.github.com>
2024-08-10 22:52:24 +0300

    feat: add loader and change click handler

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,22 +1,41 @@
-import { Outlet, useSearchParams } from '@remix-run/react';
-import type { ReactNode } from 'react';
+import { Outlet, useNavigation, useSearchParams } from '@remix-run/react';
+import { type MouseEvent, type ReactNode, useRef } from 'react';
 
+import type { FetchCharacterListResult } from '@/api/api';
 import { SearchParams } from '@/common/enums';
 import { CardList } from '@/components/card-list/CardList';
 import { ThrowErrorButton } from '@/components/error-button/ThrowErrorButton';
 import { Header } from '@/components/header/Header';
+import { Loader } from '@/components/loader/Loader';
 import { SearchForm } from '@/components/search-form/SearchForm';
 import { ThemeButton } from '@/components/theme-button/ThemeButton';
+import { DEFAULT_PAGE } from '@/store/api/api-slice';
 
 import styles from './MainPage.module.scss';
 
-export function MainPage(): ReactNode {
+export function MainPage({ characters }: { characters: FetchCharacterListResult }): ReactNode {
   const [searchParams, setSearchParams] = useSearchParams();
+  const navigation = useNavigation();
 
+  const mainRef = useRef(null);
+  const sectionRef = useRef(null);
+  const listRef = useRef(null);
+
+  const searchTerm = searchParams.get(SearchParams.NAME) || '';
+  const page = (Number(searchParams.get(SearchParams.PAGE)) || DEFAULT_PAGE).toString();
   const detailsParam = searchParams.get(SearchParams.DETAILS);
 
-  const handleMainClick = (): void => {
-    if (detailsParam) {
+  const hasPageChanged =
+    navigation.location && new URLSearchParams(navigation.location.search).get(SearchParams.PAGE) !== page;
+  const hasSearchTermChanged =
+    navigation.location && new URLSearchParams(navigation.location.search).get(SearchParams.NAME) !== searchTerm;
+  const isCardListLoading = hasPageChanged || hasSearchTermChanged;
+
+  const handleMainClick = (evt: MouseEvent): void => {
+    const isTargetRef =
+      evt.target === mainRef.current || evt.target === sectionRef.current || evt.target === listRef.current;
+
+    if (detailsParam && isTargetRef) {
       searchParams.delete(SearchParams.DETAILS);
       setSearchParams(searchParams);
     }
@@ -29,9 +48,9 @@ export function MainPage(): ReactNode {
         <ThrowErrorButton />
         <ThemeButton />
       </Header>
-      <main className={styles.main} onClick={handleMainClick}>
-        <section className={styles.listSection}>
-          <CardList />
+      <main className={styles.main} onClick={handleMainClick} ref={mainRef}>
+        <section className={styles.listSection} ref={sectionRef}>
+          {isCardListLoading ? <Loader /> : <CardList characters={characters} />}
         </section>
         {detailsParam && (
           <section className={styles.detailsSection}>
