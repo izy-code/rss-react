870d0dfcb06c11303b6105bd9c116ef8a98808a8
Izy <126877709+izy-code@users.noreply.github.com>
2024-08-10 22:53:20 +0300

    fix: use props instead of redux query

--- src/components/card-list/CardList.tsx
+++ src/components/card-list/CardList.tsx
@@ -1,58 +1,30 @@
-import { useSearchParams } from '@remix-run/react';
 import type { ReactNode } from 'react';
-import { useEffect, useRef } from 'react';
+import { forwardRef } from 'react';
 
-import { SearchParams } from '@/common/enums';
+import type { FetchCharacterListResult } from '@/api/api';
 import { Card } from '@/components/card/Card';
-import { Loader } from '@/components/loader/Loader';
-import { DEFAULT_PAGE, useGetCharactersListQuery } from '@/store/api/api-slice';
 
 import { Flyout } from '../flyout/Flyout';
 import { Pagination } from '../pagination/Pagination';
 import styles from './CardList.module.scss';
 
-export function CardList(): ReactNode {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const listRef = useRef(null);
-
-  const pageParam = Number(searchParams.get(SearchParams.PAGE));
-  const nameParam = searchParams.get(SearchParams.NAME) ?? '';
-
-  const {
-    data: characterListData,
-    isFetching,
-    isSuccess,
-    isError,
-    error,
-  } = useGetCharactersListQuery({
-    searchTerm: nameParam,
-    page: pageParam,
-  });
-
-  useEffect(() => {
-    if (!Number.isInteger(pageParam) || pageParam < DEFAULT_PAGE) {
-      searchParams.set(SearchParams.PAGE, DEFAULT_PAGE.toString());
-      setSearchParams(searchParams);
-    }
-  }, [pageParam, searchParams, setSearchParams]);
-
+export const CardList = forwardRef<HTMLUListElement, { characters: FetchCharacterListResult }>(function CardList(
+  { characters },
+  listRef,
+): ReactNode {
   let content: ReactNode = null;
 
-  if (isFetching) {
-    content = <Loader />;
-  } else if (isError) {
-    if ('status' in error && error.status === 404) {
-      content = <div className={styles.message}>No characters found</div>;
-    } else {
-      content = <div className={styles.message}>Characters list fetching problem</div>;
-    }
-  } else if (isSuccess && characterListData) {
+  if (characters.status === 'error') {
+    content = <div className={styles.message}>Characters list fetching problem</div>;
+  } else if (characters.status === 'empty' || !characters.data) {
+    content = <div className={styles.message}>No characters found</div>;
+  } else {
     content = (
       <>
-        <Pagination pageInfo={characterListData.info} />
+        <Pagination pageInfo={characters.data.info} />
         <ul className={styles.list} ref={listRef}>
-          {characterListData.results.map((characterData) => (
-            <Card key={characterData.id} character={characterData} />
+          {characters.data.results.map((character) => (
+            <Card key={character.id} character={character} />
           ))}
         </ul>
         <Flyout />
@@ -61,4 +33,4 @@ export function CardList(): ReactNode {
   }
 
   return content;
-}
+});
